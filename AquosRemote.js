146d29c9c66f8f723c85c52ae3b34dcb9726dcf5
Phil Bri <ph.brioude@gmail.com>
2015-06-21 16:55:53 +0200

    modif

--- AquosRemote.js
+++ AquosRemote.js
@@ -1,13 +1,3 @@
-/*_______________________________________________________
-|                 AquosRemote v0.1                       |
-|                                                        |
-| Authors : Jérôme Garcia Miranda & Phil Bri ( 01/2015 ) |
-| Description :                                          |
-|    Sharp Aquos TV's Plugin for SARAH project           |
-|    (See http://encausse.wordpress.com/s-a-r-a-h/)      |
-|________________________________________________________|
-*/
-
 exports.action = function ( data , callback , config , SARAH ) {
 
 	var net = require ( 'net' ),
@@ -21,45 +11,47 @@ exports.action = function ( data , callback , config , SARAH ) {
 
 	Aquos.IP 		= cfg.IP,
 	Aquos.Port 		= ( typeof cfg.Port != 'undefined' ) ? cfg.Port : 10002,
-	Aquos.User 		= ( typeof cfg.User != 'undefined' ) ? cfg.User : 'admin',
-	Aquos.password 	= ( typeof cfg.password != 'undefined' ) ? cfg.password : 'admin';
+	Aquos.Username	= ( typeof cfg.User != 'undefined' ) ? cfg.User : false,
+	Aquos.password 	= ( typeof cfg.password != 'undefined' ) ? cfg.password : false;
+
+	if (!Aquos.Username || !Aquos.password) {
+		console.log ( '\nAquosRemote [Erreur] => User et/ou Password absents' );
+		return callback ({ "tts" : "Erreur d'authentification. Nom d'utilisateur et ou mot de passe absent." });
+	}
 	
-	data.cmd = data.cmd.concat( '    ' ).substr(0,8) + '\x0D';
+	data.cmd = data.cmd.concat( '        ' ).substr(0,8) + '\x0D';
 
 	var socket = net.connect ({ host: Aquos.IP, port: Aquos.Port });
 
 	socket.on ( 'connect', function() {
 		console.log ( '\nAquosRemote => Connexion = [OK] => user: '+Aquos.Username+' - Password: '+Aquos.password);
-		socket.write ( Aquos.Username + "\n" + Aquos.password + "\n" );
-	});
+		
+		socket.on ( 'data', function ( data ) {
+			aquosData = data.toString();
+			// Debug
+			console.log ( '\n--------- Valeur à communiquer (ci-dessous) ----------');
+			console.log (aquosData);
+			console.log ( '------------------------------------------------------\n');
+			// End debug
 
-	socket.on ( 'data', function ( data ) {
-		Aquos.Data = data.toString();
-
-		// Debug
-		console.log ( '\n--------- Valeur à communiquer (ci-dessous) ----------');
-		console.log (Aquos.Data);
-		console.log ( '------------------------------------------------------\n');
-		// End debug
-
-		if ( Aquos.Data.indexOf( "Login" ) != -1 ) {
-			if ( !Aquos.User || !Aquos.password ) {
-				socket.destroy();
-				console.log ( '\nAquosRemote [Erreur] => User et/ou Password absents' );
-				return callback ({ "tts" : "Erreur d'authentification. Nom d'utilisateur et ou mot de passe absent." });
+			if ( aquosData.indexOf( "Login" ) != -1 ) {
+				socket.write ( Aquos.Username + "\n" + Aquos.password + "\n" );
+				console.log('AquosRemote : User + Login envoyés...');
+				socket.write("RSPW2   \x0D"); // commente cette ligne avec // devant...
+				console.log('AquosRemote : RSPW2 envoyé...');
 			}
-			else socket.write ( Aquos.Username + "\n" + Aquos.password + "\n" );
-		}
-		if ( Aquos.Data.indexOf( "OK" ) != -1 ) {
-			socket.end ( data.cmd );
-			console.log ( '\nAquosRemote => Cmd = [OK]' );
-			callback ({ "tts" : data.ttsAction });
-		}
-		if ( Aquos.Data.indexOf( "User Name or Password mismatch" ) != -1 ) {
-			socket.destroy();
-			console.log ( '\nAquosRemote [Erreur] => User et/ou Password incorrects' );
-			return callback ({ "tts" : "Erreur d'authentification. Nom d'utilisateur et ou mot de passe incorrects." });
-		}
+			if ( aquosData.indexOf( "OK" ) != -1 ) {
+				console.log('AquosRemote : Login OK');
+				socket.end ( data.cmd );
+				console.log ( '\nAquosRemote => Cmd: '+data.cmd+' = [OK]' );
+				callback ({ "tts" : data.ttsAction });
+			}
+			if ( aquosData.indexOf( "User Name or Password mismatch" ) != -1 ) {
+				socket.destroy();
+				console.log ( '\nAquosRemote [Erreur] => User et/ou Password incorrects' );
+				return callback ({ "tts" : "Erreur d'authentification. Nom d'utilisateur et ou mot de passe incorrects." });
+			}
+		});
 	});
 
 	socket.on ( 'error', function ( erreur ) {
